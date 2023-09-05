a4bbe0e991b1f6a4636a5610d39d928f20c3b7a3
gb <glumbongo93@gmail.com>
2023-09-05 14:22:34 +0100

    added shader loader that is passed a shader object variable at the top of the file

--- main.js
+++ main.js
@@ -1,22 +1,53 @@
 import * as THREE from "three";
 
+//object passed to loadShader that details shader file locations
+//any uniforms declared in the shader files must be passed in via the
+//uniforms object  
+const shaders = {
+    vertexFile: null,
+    fragmentFile: '/shaders/hello-world/hello-world.frag',
+    uniforms: {},
+}
 
-//pass vertex shader file, fragment file, and uniforms object 
-const material = loadShader();
+//function that returns a promise to load a shader file or null if the
+//shaderfile path is empty
+function loadShaderFile(shaderFilePath) {
+    return shaderFilePath ? (
+    fetch(shaderFilePath)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to load shader file');
+        }
+        return response.text();
+    })
+    ) : null;
+}
 
-
-function loadShader(vertexFile, fragmentFile, uniforms) { 
+//async function that loads shader filess with promises, initializes and
+//returns a new shader material 
+async function createShaderMaterial(shaderFiles = {}) {     
+    const vertexShader = await loadShaderFile(shaderFiles.vertexFile);
+    console.log("Vertex shader \n ----- \n\n", vertexShader);
+    const fragmentShader = await loadShaderFile(shaderFiles.fragmentFile);
+    console.log("Fragment shader \n ----- \n\n", fragmentShader);
+    const uniforms = shaderFiles.uniforms;  
     //shader parameters object
+    //spread a conditional object into the master object
     const shaderParameters = {
-
-        uniforms: {
-
-        },
-        vertexShader: null,  //frag files need to be loaded as a string 
-        fragmentShader: null
-    }
+        ...(fragmentShader ? { fragmentShader } : {}), 
+        ...(vertexShader ? { vertexShader } : {}),
+        uniforms,
+    };
     return new THREE.ShaderMaterial(shaderParameters);
 }
+
+
+//pass vertex shader file, fragment file, and uniforms object 
+const material = await createShaderMaterial(shaders);
+console.log("initialized shader material \n ------ \n", material);
+
+
+
 //initialize renderer, camera, scene
 const canvas = document.getElementById('c');
 const renderer = new THREE.WebGLRenderer({canvas,antialias: true});
@@ -29,12 +60,16 @@ renderer.a
 //create plane mesh and cover screen, attach shader as material, add to scene 
 const geometry = new THREE.PlaneGeometry(2,2);
 //filler material for debugging (doesnt need light)
-const tempMaterial = new THREE.MeshBasicMaterial({color: 'red'});
+/* const tempMaterial = new THREE.MeshBasicMaterial({color: 'red'}); */
 
 
-const mesh = new THREE.Mesh(geometry, tempMaterial);
+const mesh = new THREE.Mesh(geometry,material);
+console.log("initialized mesh\n------\n", mesh);
+
+
 scene.add(mesh);
 
+console.log("scene object before animation loop begins \n------\n", scene);
 
 //run animation loop 
 const animate = () => {
--- shaders/hello-world/hello-world.frag
+++ shaders/hello-world/hello-world.frag
@@ -1,9 +1,3 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-uniform float u_time;
-
 void main() {
-	gl_FragColor = vec4(1.000,0.502,0.137,1.000);
+	gl_FragColor = vec4(0.234,0.502,0.137,1.000);
 }
